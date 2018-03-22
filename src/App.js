7343eb0fad7342ffa367a90f87e371e8abef4bfe
arekoleksik <arkadiuszoleksik@gmail.com>
2018-03-22 20:01:29 +0100

    Login to app - view form

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import TopNav from './components/nav.components/top.nav.component/top.nav.component'
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import SideMenu from './components/nav.components/side.nav.component/side.nav.component'
+import LoginForm from './components/login.components/login.component'
 
 class App extends Component {
     render() {
@@ -17,6 +18,7 @@ class App extends Component {
                     </MuiThemeProvider>
                 </header>
                 <main>
+                    <LoginForm/>
                 </main>
             </div>
         );
