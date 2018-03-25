f160ead50776c4a54ee983707ec9f7cf0d18b102
Andrzej Kublicki <pionier2000@wp.pl>
2018-03-25 13:49:16 +0200

    List of search results init

--- src/components/search.component/TableResults.component.js
+++ src/components/search.component/TableResults.component.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import Checkbox from 'material-ui/Checkbox';
+import ActionFavorite from 'material-ui/svg-icons/action/favorite';
+import ActionFavoriteBorder from 'material-ui/svg-icons/action/favorite-border';
 import {
     Table,
     TableBody,
@@ -19,55 +22,78 @@ const styles = {
     },
 };
 
+const check = (e, isChecked) => {
+    console.log(e.target.value);
+    if(isChecked){
+        console.log('dodane do ulubionych');
+    }else{
+        console.log('usuniete z ulubionych');
+    }
+};
+
 const TableResults = (props) => {
 
     return (
         <div>
             <Table
-                onCellClick={(row, col, event) => console.log(props.tableData[row])}
-                height={'65vh'}
-                fixedHeader={true}
-                fixedFooter={true}
-                selectable={true}
-                multiSelectable={false}
-                style={{
-                    marginLeft: '256px',
-                    width: '70%',
+                onCellClick={(row, col, event) => {
+                    if (!event.target.value) {
+                        console.log(props.tableData[row].code);
+                    }
                 }}
+            height={'65vh'}
+            fixedHeader={true}
+            fixedFooter={true}
+            selectable={true}
+            multiSelectable={false}
+            style={{
+                marginLeft: '256px',
+                width: '70%',
+            }}
+        >
+            <TableHeader
+                displaySelectAll={false}
+                adjustForCheckbox={false}
+                enableSelectAll={false}
             >
-                <TableHeader
-                    displaySelectAll={false}
-                    adjustForCheckbox={false}
-                    enableSelectAll={false}
-                >
-                    <TableRow>
-                        <TableHeaderColumn colSpan="3" style={{textAlign: 'center'}}>
-                            {props.tableName}
-                        </TableHeaderColumn>
+                <TableRow>
+                    <TableHeaderColumn colSpan="3" style={{textAlign: 'center'}}>
+                        {props.tableName}
+                    </TableHeaderColumn>
+                </TableRow>
+                <TableRow>
+                    <TableHeaderColumn>Nazwa</TableHeaderColumn>
+                    <TableHeaderColumn>Kod</TableHeaderColumn>
+                    <TableHeaderColumn>Wartość (PLN)</TableHeaderColumn>
+                    <TableHeaderColumn>Ulubione</TableHeaderColumn>
+                </TableRow>
+            </TableHeader>
+            <TableBody
+                displayRowCheckbox={false}
+                deselectOnClickaway={true}
+                showRowHover={true}
+                stripedRows={false}
+            >
+                {props.tableData.map((row, index) => (
+                    <TableRow key={index}>
+                        <TableRowColumn>{row.currency}</TableRowColumn>
+                        <TableRowColumn>{row.code}</TableRowColumn>
+                        <TableRowColumn>{row.mid}</TableRowColumn>
+                        <TableRowColumn>
+                            <Checkbox
+                                checkedIcon={<ActionFavorite/>}
+                                uncheckedIcon={<ActionFavoriteBorder/>}
+                                style={styles.checkbox}
+                                onCheck={check}
+                            />
+                        </TableRowColumn>
                     </TableRow>
-                    <TableRow>
-                        <TableHeaderColumn>Nazwa</TableHeaderColumn>
-                        <TableHeaderColumn>Kod</TableHeaderColumn>
-                        <TableHeaderColumn>Wartość (PLN)</TableHeaderColumn>
-                    </TableRow>
-                </TableHeader>
-                <TableBody
-                    displayRowCheckbox={false}
-                    deselectOnClickaway={true}
-                    showRowHover={true}
-                    stripedRows={false}
-                >
-                    {props.tableData.map((row, index) => (
-                        <TableRow key={index}>
-                            <TableRowColumn>{row.currency}</TableRowColumn>
-                            <TableRowColumn>{row.code}</TableRowColumn>
-                            <TableRowColumn>{row.mid}</TableRowColumn>
-                        </TableRow>
-                    ))}
-                </TableBody>
-            </Table>
-        </div>
-    );
+                ))}
+            </TableBody>
+        </Table>
+</div>
+)
+    ;
 }
 
 export default TableResults;
