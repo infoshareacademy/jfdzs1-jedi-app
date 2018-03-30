bdb6cacae45cefa494ff5656eca65af06e24805d
Andrzej Kublicki <pionier2000@wp.pl>
2018-03-30 21:04:24 +0200

    Geochart init

--- src/index.js
+++ src/index.js
@@ -3,12 +3,14 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import SearchItem from './routes/search/search.component';
+import WorldMap from './routes/world.currency.map/world.currency.map.component';
 import {BrowserRouter, Route} from 'react-router-dom';
 
 ReactDOM.render(
     <BrowserRouter>
         <div>
             <Route exact path="/" component={App} />
+            <Route exact path="/map" component={WorldMap} />
             <Route path="/search" component={SearchItem} />
         </div>
     </BrowserRouter>,
