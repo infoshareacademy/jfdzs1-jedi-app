f006ef8b43e4178dce5dcc142f576b1a7aeb2337
Andrzej Kublicki <pionier2000@wp.pl>
2018-04-07 09:43:06 +0200

    add ServiceWorker

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import App from './App';
 import SearchItem from './routes/search/search.component';
 import WorldMap from './routes/world.currency.map/world.currency.map.component';
 import {BrowserRouter, Route} from 'react-router-dom';
+import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(
     <BrowserRouter>
@@ -17,3 +18,4 @@ ReactDOM.render(
     document.getElementById('root')
 );
 
+registerServiceWorker();
